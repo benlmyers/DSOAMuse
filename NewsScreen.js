02606d6f60d28b9f1a88a2edf8ae44c090df759a
Ben Myers <benben28405678@gmail.com>
2019-08-20 12:59:50 -0400

    Added article content.

--- NewsScreen.js
+++ NewsScreen.js
@@ -46,7 +46,7 @@ export default class NewsScreen extends React.Component {
       isLoading: true,
       dataSource: [],
       titles: [],
-      fadeAnim: [new Animated.Value(0)],
+      fadeAnim: new Animated.Value(0),
     }
   }
 
@@ -60,10 +60,10 @@ export default class NewsScreen extends React.Component {
       .then((responseJson) => {
 
         Animated.timing(
-          this.state.fadeAnim[0],
+          this.state.fadeAnim,
           {
             toValue: 1,
-            duration: 1000,
+            duration: 500,
           }
         ).start();
 
@@ -72,6 +72,7 @@ export default class NewsScreen extends React.Component {
           dataSource: responseJson,
           articleTitle: responseJson[0].title.rendered,
           articleBanner: responseJson[0].featured_image_urls.large,
+          articleContent: responseJson[0].content.rendered,
         }, function(){
 
         });
@@ -103,10 +104,17 @@ export default class NewsScreen extends React.Component {
           <StatusBar barStyle="dark-content">
           </StatusBar>
           <SafeAreaView>
-            <View style={styles.bgImageWrapper}>
-              <Image source={{uri: this.state.articleBanner}} style={styles.bgImage}/>
-            </View>
-            <Text>{this.state.articleTitle}</Text>
+            <ScrollView>
+              <Animated.View style={{opacity: this.state.fadeAnim}}>
+                <View style={styles.bgImageWrapper}>
+                  <Image source={{uri: this.state.articleBanner}} style={styles.bgImage}/>
+                </View>
+                <Text style={styles.title}>{toTitleCase(this.state.articleTitle)}</Text>
+                <Text style={styles.content}>
+                  {unescapeHTML(this.state.articleContent)}
+                </Text>
+              </Animated.View>
+            </ScrollView>
           </SafeAreaView>
         </Fragment>
     );
@@ -114,16 +122,88 @@ export default class NewsScreen extends React.Component {
 
 }
 
+function toTitleCase(str) {
+  if(str == '' || str == null) {
+    return;
+  }
+  str = str.replace("&#8220;", '"')
+  str = str.replace("&#8216;", "'")
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return unescapeHTML(txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+    }
+  );
+}
+
+function unescapeHTML(str) {
+
+  if(str == '' || str == null) {
+    return;
+  }
+  var escapeChars = { lt: '<', gt: '>', quot: '"', apos: "'", amp: '&' };
+  str = str.replace(/<\/?[^>]+(>|$)/g, "");
+  str = str.replace("&nbsp; ", "");
+  return str.replace(/\&([^;]+);/g, function(entity, entityCode) {
+    var match;
+
+    if ( entityCode in escapeChars) {
+      return escapeChars[entityCode];
+    } else if ( match = entityCode.match(/^#x([\da-fA-F]+)$/)) {
+      return String.fromCharCode(parseInt(match[1], 16));
+    } else if ( match = entityCode.match(/^#(\d+)$/)) {
+      return String.fromCharCode(~~match[1]);
+    } else {
+      return entity;
+    }
+  });
+}
+
+function simpleDate(str) {
+  if(str == '' || str == null) {
+    return;
+  }
+  var month = str.slice(5, 7);
+  switch(month) {
+    case "01": month = "JAN"; break;
+    case "02": month = "FEB"; break;
+    case "03": month = "MAR"; break;
+    case "04": month = "APR"; break;
+    case "05": month = "MAY"; break;
+    case "06": month = "JUN"; break;
+    case "07": month = "JUL"; break;
+    case "08": month = "AUG"; break;
+    case "09": month = "SEP"; break;
+    case "10": month = "OCT"; break;
+    case "11": month = "NOV"; break;
+    case "12": month = "DEC"; break;
+  }
+
+  var day = str.slice(8, 10);
+
+  return month + " " + parseInt(day);
+}
+
 const screenWidth = Math.round(Dimensions.get('window').width);
 
 var styles = StyleSheet.create({
   bgImageWrapper: {
-    position: 'absolute',
     top: 0, bottom: 0, left: 0, right: 0,
     height: 250,
   },
   bgImage: {
     flex: 1,
     resizeMode: "stretch"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: Colors.black,
+    textAlign: 'center',
+    padding: 20,
+  },
+  content: {
+    padding: 5,
+    fontSize: 14,
   },
 });
