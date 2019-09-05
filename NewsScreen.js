761791499ce43bc8c68b1ff0d68bb9d93857d42a
Ben Myers <benben28405678@gmail.com>
2019-09-05 13:43:40 -0400

    First steps to video.

--- NewsScreen.js
+++ NewsScreen.js
@@ -29,6 +29,9 @@ import {
 
 import { WebView } from 'react-native-webview';
 
+import Video from 'react-native-video';
+import AnimVideo from "./anim.mp4";
+
 import {
   Header,
   LearnMoreLinks,
@@ -117,6 +120,17 @@ export default class NewsScreen extends React.Component {
           </StatusBar>
           <SafeAreaView>
             <ScrollView>
+
+            <View style={styles.container}>
+            <Video source={AnimVideo}   // Can be a URL or a local file.
+             ref={(ref) => {
+               this.player = ref
+             }}                                      // Store reference
+             onBuffer={this.onBuffer}                // Callback when remote video is buffering
+             onError={this.videoError}               // Callback when video cannot be loaded
+             style={styles.backgroundVideo} />
+            </View>
+
               <Animated.View style={{opacity: this.state.fadeAnim[0]}}>
                 <Animated.View style={{opacity: this.state.fadeAnim[1]}}>
                   <View style={styles.bgImageWrapper}>
@@ -261,5 +275,15 @@ var styles = StyleSheet.create({
     fontSize: 14,
     height: 5000,
     fontFamily: 'system font'
+  },
+  container: {
+    flex: 1,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
--- android/app/src/main/java/com/dsoamuse/MainApplication.java
+++ android/app/src/main/java/com/dsoamuse/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
